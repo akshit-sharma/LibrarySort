3f5c8a43bfeb936d12b02233b2d4b8899d4453f9
akshit-sharma <akshit9sharma@gmail.com>
2017-02-22 18:15:19 +0530

    to do modify DS

---
+++ LibrarySort/SchemeDataStructure.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+#include <string>
+
+class SchemeDataStructure
+{
+	int * scheme_prog_code;
+	std::string * prog_name;
+	int * scheme_id;
+	std::string * prog_sem_year;
+	std::string * prepared_date;
+	std::string * declared_date;
+	int * institution_code;
+	std::string * institution_name;
+	int * s_number;
+	int * paper_id;
+	std::string * paper_code;
+	std::string * subject_name;
+	int * credits;
+	std::string * type;
+	std::string * exam;
+	std::string * mode;
+	std::string * kind;
+	std::string * minor;
+	std::string * major;
+	std::string * max_marks;
+	std::string * pass_marks;
+
+public:
+	void swap(size_t index_1, size_t index_2);
+
+};
